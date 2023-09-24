304b9c57d894d391941670d6f62e2d4698b08e69
zihadsikder <zihadsikder.cse@gmail.com>
2023-09-24 23:13:52 +0600

    item_count

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,3 @@
-
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Shopping App',
-//       home: ProductList(),
-//     );
-//   }
-// }
-//
-// class ProductList extends StatefulWidget {
-//   @override
-//   _ProductListState createState() => _ProductListState();
-// }
-//
-// class _ProductListState extends State<ProductList> {
-//   List<Product> products = [
-//     Product(name: "Product A", price: 10),
-//     Product(name: "Product B", price: 15),
-//     Product(name: "Product C", price: 20),
-//   ];
-//
-//   List<Product> selectedProducts = [];
-//
-//   void addToCart(int index) {
-//     setState(() {
-//       products[index].quantity++;
-//       if (products[index].quantity == 5) {
-//         showCongratsDialog(products[index]);
-//       }
-//       selectedProducts.add(products[index]);
-//     });
-//   }
-//
-//   void showCongratsDialog(Product product) {
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text('Congratulations!'),
-//           content: Text('You\'ve bought 5 ${product.name}!'),
-//           actions: [
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.of(context).pop();
-//               },
-//               child: Text('OK'),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-//
-//   void goToCartPage() {
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(
-//         builder: (context) => CartPage(selectedProducts: selectedProducts),
-//       ),
-//     );
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Center(child: Text('Product List')),
-//       ),
-//       body: ListView.builder(
-//         itemCount: products.length,
-//         itemBuilder: (context, index) {
-//           return ListTile(
-//             title: Text(products[index].name),
-//             subtitle: Text('Price: \$${products[index].price}'),
-//             trailing: Row(
-//               mainAxisSize: MainAxisSize.min,
-//               children: [
-//                 SingleChildScrollView(
-//                   child: Column(
-//                     children: [
-//                       Text('Count: ${products[index].quantity} '),
-//                       ElevatedButton(
-//                         onPressed: () => addToCart(index),
-//                         child: Text('Buy Now'),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           );
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: goToCartPage,
-//         child: Icon(Icons.shopping_cart),
-//       ),
-//     );
-//   }
-// }
-//
-// class Product {
-//   final String name;
-//   final int price;
-//   int quantity;
-//
-//   Product({required this.name, required this.price, this.quantity = 0});
-// }
-//
-// class CartPage extends StatelessWidget {
-//   final List<Product> selectedProducts;
-//
-//   CartPage({required this.selectedProducts});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     int selectedProductCount = selectedProducts.length;
-//
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Center(child: Text('Cart')),
-//       ),
-//       body: Center(
-//         child: Text('You have selected $selectedProductCount products.'),
-//       ),
-//     );
-//   }
-// }
-//
-//
-//
-//
 import 'package:flutter/material.dart';
 
 void main() {
@@ -148,43 +8,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shopping App',
-      home: ProductList(),
+      title: 'Counting App',
+      debugShowCheckedModeBanner: false,
+      home: CountingScreen(),
     );
   }
 }
 
-class ProductList extends StatefulWidget {
+class CountingScreen extends StatefulWidget {
   @override
-  _ProductListState createState() => _ProductListState();
+  _CountingScreenState createState() => _CountingScreenState();
 }
 
-class _ProductListState extends State<ProductList> {
-  List<Product> products = [
-    Product(name: "Walton Monitor", price: 20000),
-    Product(name: "HP Laptop", price: 15000),
-    Product(name: "Iphone 15", price: 200000),
-  ];
+class _CountingScreenState extends State<CountingScreen> {
+  int count = 0;
 
-  List<Product> selectedProducts = [];
-
-  void addToCart(int index) {
+  void incrementCount() {
     setState(() {
-      products[index].quantity++;
-      if (products[index].quantity == 5) {
-        showCongratsDialog(products[index]);
+      count++;
+      if (count >= 5) {
+        showCountDialog(count);
       }
-      selectedProducts.add(products[index]);
     });
   }
 
-  void showCongratsDialog(Product product) {
+  void decrementCount() {
+    setState(() {
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
+
+  void showCountDialog(int count) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Congratulations!'),
-          content: Text('You\'ve bought 5 ${product.name}!'),
+          title: Text('Button pressed $count times'),
           actions: [
             TextButton(
               onPressed: () {
@@ -198,80 +59,38 @@ class _ProductListState extends State<ProductList> {
     );
   }
 
-  void goToCartPage() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CartPage(selectedProducts: selectedProducts),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Product List')),
+        title: Center(
+            child: Text('Counting App')),
       ),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(products[index].name),
-            subtitle: Text('Price: \$${products[index].price}'),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Text('Count: ${products[index].quantity} '),
-                      ElevatedButton(
-                        onPressed: () => addToCart(index),
-                        child: Text('Buy Now'),
-                      ),
-                    ],
-                  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Count: $count',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: incrementCount,
+                  child: Text('+'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: decrementCount,
+                  child: Text('-'),
                 ),
               ],
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: goToCartPage,
-        child: Icon(Icons.shopping_cart),
-      ),
-    );
-  }
-}
-
-class Product {
-  final String name;
-  final int price;
-  int quantity;
-
-  Product({required this.name, required this.price, this.quantity = 0});
-}
-
-class CartPage extends StatelessWidget {
-  final List<Product> selectedProducts;
-
-  CartPage({required this.selectedProducts});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Cart')),
-      ),
-      body: ListView.builder(
-        itemCount: selectedProducts.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(selectedProducts[index].name),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
