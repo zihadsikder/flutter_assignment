87e4b9820b2b4e130d098230325f61af7aeecc26
zihadsikder <zihadsikder.cse@gmail.com>
2023-10-07 18:27:10 +0600

    shopping card list price

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter app',
       debugShowCheckedModeBanner: false,
-      home: SizeSelector(),
-    );
-  }
-}
-
-class SizeSelector extends StatefulWidget {
-  @override
-  _SizeSelectorState createState() => _SizeSelectorState();
-}
-
-class _SizeSelectorState extends State<SizeSelector> {
-  String selectedSize = '';
-  final List<String> sizes = ['X', 'L', 'M', 'XL', 'XXL', 'XXXL'];
-
-  void _onSizeButtonPressed(String size) {
-    setState(() {
-      selectedSize = size;
-    });
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Selected Size: $size'),
-      ),
-    );
-  }
-
-  ElevatedButton _buildSizeButton(String size) {
-    final isSelected = selectedSize == size;
-    return ElevatedButton(
-      onPressed: () => _onSizeButtonPressed(size),
-      child: Text(size),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(
-          isSelected ? Colors.green : Colors.blue,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Select Size'),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Center(
-              child: ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: [
-                  for (int i = 0; i < 4; i++) _buildSizeButton(sizes[i]),
-                ],
-              ),
-            ),
-            Center(
-              child: ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: [
-                  for (int i = 4; i < sizes.length; i++) _buildSizeButton(sizes[i]),
-                ],
-              ),
-            ),
-          ],
+      home: HomePage(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
         ),
       ),
     );
   }
 }
+
