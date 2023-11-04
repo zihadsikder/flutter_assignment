815484e2c5be6f96cf1cfabbffc1dac0c504ab67
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-04 15:25:14 +0600

    Assignment 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,129 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-import 'home_page.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          color: Colors.white,
-          elevation: 0,
-        ),
+      home: PhotoListScreen(),
+    );
+  }
+}
+
+class Photo {
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+}
+
+class PhotoListScreen extends StatefulWidget {
+  @override
+  _PhotoListScreenState createState() => _PhotoListScreenState();
+}
+
+class _PhotoListScreenState extends State<PhotoListScreen> {
+  late Future<List<Photo>> photos;
+
+  Future<List<Photo>> fetchPhotos() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/photos'),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      List<Photo> photos = data.map((json) => Photo.fromJson(json)).toList();
+      return photos;
+    } else {
+      throw Exception('Failed to load photos');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    photos = fetchPhotos();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo List'),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: photos,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Photo> photoList = snapshot.data!;
+            return ListView.builder(
+              itemCount: photoList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Image.network(photoList[index].thumbnailUrl),
+                  title: Text(photoList[index].title),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PhotoDetailScreen(photoList[index]),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
 }
 
+class PhotoDetailScreen extends StatelessWidget {
+  final Photo photo;
+
+  PhotoDetailScreen(this.photo);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Details'),
+      ),
+      body: Column(
+        children: [
+          Image.network(photo.url),
+          Text('ID: ${photo.id}'),
+          Text('Title: ${photo.title}'),
+        ],
+      ),
+    );
+  }
+}
